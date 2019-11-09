a783a717354d83c3fd7cf05ca32a3bc356fc5428
Mykola1981 <mykola1981@gmail.com>
2019-11-09 22:13:09 +0200

    design

--- src/componnents/header/headerComponnent.js
+++ src/componnents/header/headerComponnent.js
@@ -6,15 +6,16 @@ import "./headerComponnent.css";
 const Header = () => {
     return(
         <div className="row">
+            <div className="col text-center">
             <ul>
-                <li>
-                    <Link  to="/">Home </Link>
-                </li>
-                <li>
-                    <Link  to="/add">Add new contact </Link>
+                <li className="text-center">
+
+
+                    <Link  to="/add"><i className="fa fa-mobile"></i>Add new contact </Link>
                 </li>
             </ul>
             <h2 className="text-center" ><span>Contact List</span>Created with <i className="fa fa-heart"></i> from me</h2>
+            </div>
         </div>
     );
 }
