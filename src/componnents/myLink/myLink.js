7a3c14207c132e53ed5341a9eecd01147894c7bf
Mykola1981 <mykola1981@gmail.com>
2019-11-10 23:31:15 +0200

    back to list

--- src/componnents/myLink/myLink.js
+++ src/componnents/myLink/myLink.js
@@ -5,15 +5,23 @@ import { Link } from "react-router-dom";
 
 class MyLink extends Component{
 
+    state = {
+        switchActions: ["add", "list"],
+        linkTexts: ["Add new contact", "Back to contact list"],
+        screenNum: 0
+    };
     switchTo = (e) => {
         e.preventDefault();
-        this.props.switchTo('add');
+        this.props.switchTo(this.state.switchActions[this.state.screenNum]);
+        this.setState({
+            screenNum: 1 - this.state.screenNum
+        })
     };
 
     render(){
 
         return(
-            <Link  to="/" onClick={this.switchTo}><i className="fa fa-mobile"></i>Add new contact </Link>
+            <Link  to="/" onClick={this.switchTo}>{this.state.linkTexts[this.state.screenNum]}</Link>
         )
     }
 }
